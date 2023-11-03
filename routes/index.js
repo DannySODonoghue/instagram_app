6f1a39f1154470265a7d70b2184ae183d8f5cbab
Daniel O'Donoghue <dannyod14@gmail.com>
2023-11-03 18:54:46 -0400

    testing

--- routes/index.js
+++ routes/index.js
@@ -36,15 +36,15 @@ router.get('/user/auth/', async (req, res) => {
     // bodyFormData.append('grant_type', 'authorization_code');
     // bodyFormData.append('redirect_uri', 'https://instaunfollowers-dc8e3299f8e9.herokuapp.com/user/auth/');
     // bodyFormData.append('code', code);
-    const URL = "https://api.instagram.com/oauth/access_token";
+    const url = "https://api.instagram.com/oauth/access_token";
     
     try {
-        const response = await axios.post(URL, qs.stringify(values), {
-            headers: {
-                "Content-Type": 'application/x-www-form-urlencoded'
-            },
+        const response = await axios.post({
+            url,
+            data: values
         });
-        res.send({code});
+        console.log('here');
+        res.send(response);
     } catch (error) {
         console.log('here4');
         res.send({in: "error"});
