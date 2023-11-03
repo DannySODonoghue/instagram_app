167a17c4e0baa3c3e039d287a5ab8f7ba9e4b3a4
Daniel O'Donoghue <dannyod14@gmail.com>
2023-11-03 18:44:08 -0400

    mem

--- routes/index.js
+++ routes/index.js
@@ -22,38 +22,41 @@ router.get('/', (req, res) => {
 router.get('/user/auth/', async (req, res) => {
     console.log('here');
     const code = req.query.code;
-    const values = {
-        client_id: CLIENT_ID,
-        client_secret: CLIENT_SECRET,
-        grant_type: 'authorization_code',
-        redirect_uri: 'https://instaunfollowers-dc8e3299f8e9.herokuapp.com/user/auth/',
-        code
-    }
+    res.send({code});
+
+    // const values = {
+    //     client_id: CLIENT_ID,
+    //     client_secret: CLIENT_SECRET,
+    //     grant_type: 'authorization_code',
+    //     redirect_uri: 'https://instaunfollowers-dc8e3299f8e9.herokuapp.com/user/auth/',
+    //     code
+    // }
     // const bodyFormData = new FormData();
     // bodyFormData.append('client_id', CLIENT_ID);
     // bodyFormData.append('client_secret', CLIENT_SECRET);
     // bodyFormData.append('grant_type', 'authorization_code');
     // bodyFormData.append('redirect_uri', 'https://instaunfollowers-dc8e3299f8e9.herokuapp.com/user/auth/');
     // bodyFormData.append('code', code);
-    const URL = "https://api.instagram.com/oauth/access_token";
-    console.log('here2');
+    // const URL = "https://api.instagram.com/oauth/access_token";
+    // console.log('here2');
     
-    try {
-        const response = await axios.post(URL, qs.stringify(values), {
-            headers: {
-                "Content-Type": 'application/x-www-form-urlencoded'
-            },
-        });
-        res.send(
-            { 
-                name: "Hello World",
-                response
-            }
-        )
-    } catch (error) {
-        console.error(error);
-        res.send(error);
-    }
+    // try {
+    //     const response = await axios.post(URL, qs.stringify(values), {
+    //         headers: {
+    //             "Content-Type": 'application/x-www-form-urlencoded'
+    //         },
+    //     });
+    //     res.send(
+    //         { 
+    //             name: "Hello World",
+    //             response
+    //         }
+    //     )
+    // } catch (error) {
+    //     console.log('here4');
+    //     console.error(error);
+    //     res.send(error);
+    // }
 })
 
 
